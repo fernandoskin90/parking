a82a8ef8e0ba2e876c287def4b949845e30ba026
fernando <fernandoguerreroc65@gmail.com>
2019-08-05 07:46:41 -0500

    config to routes, added componnets, config server, created folder app, created components react

---
+++ app/src/constants.js
@@ -0,0 +1,9 @@
+export const validateUserByToken =
+  'https://stormy-ocean-53996.herokuapp.com/user/authByToken';
+
+export const loginUrl = 'https://stormy-ocean-53996.herokuapp.com/user/login';
+
+export const registerUrl =
+  'https://stormy-ocean-53996.herokuapp.com/user/register';
+
+export const getPostsUrl = 'https://stormy-ocean-53996.herokuapp.com/posts';
